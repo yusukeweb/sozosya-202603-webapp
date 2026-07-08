00d1f0ad31a088056cb8bad61f26d04326a12895
kogoukeita-cmyk <kogoukeita@gmail.com>
2026-07-08 10:07:55 +0900

    kogou css変更

--- src/app/members/kogou/page.jsx
+++ src/app/members/kogou/page.jsx
@@ -1,12 +1,19 @@
-
-import styles from './members.module.css'
+import styles from "./members.module.css";
 
 export default function MembersPage() {
   return (
     <section className={styles.container}>
       <h1 className={styles.name}>古郷 啓大</h1>
-      <p>勉強中：React / Next.js</p>
-      <p className={styles.messeage}>GitHub共同開発自己紹介カード制作</p>
+      <div className={styles.contents}>
+        <div>
+          <h2 className={styles.about}>勉強中</h2>
+          <p>React / Next.js</p>
+        </div>
+        <div>
+          <h2 className={styles.about}>メッセージ</h2>
+          <p>GitHub共同開発自己紹介カード制作中</p>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
